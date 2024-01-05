e8d71646556f96b475998bfd898fdde6cbeb8f7a
Kacper Łęczyński <kacper.leczynski@wpengine.com>
2024-01-05 09:24:40 +0100

    init: go-stripe init

---
+++ cmd/web/handlers.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"myapp/internal/models"
+	"net/http"
+)
+
+func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
+
+	if err := app.renderTemplate(w, r, "terminal", &templateData{}, "stripe-js"); err != nil {
+		app.errorLog.Println(err)
+
+	}
+}
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("cardholder_email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["email"] = email
+	data["pi"] = paymentIntent
+	data["pm"] = paymentMethod
+	data["pa"] = paymentAmount
+	data["pc"] = paymentCurrency
+
+	if err := app.renderTemplate(w, r, "succeeded", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
+
+	}
+}
+
+func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
+	widget := models.Widget{
+		ID:              1,
+		Name:            "Custom Widget",
+		Description:     "A very nice widget",
+		InverntoryLevel: 10,
+		Price:           1000,
+	}
+
+	data := make(map[string]interface{})
+	data["widget"] = widget
+
+	stringMap := make(map[string]string)
+	stringMap["publishable_key"] = app.config.stripe.key
+	if err := app.renderTemplate(w, r, "buy-once", &templateData{Data: data, StringMap: stringMap}, "stripe-js"); err != nil {
+		app.errorLog.Println(err)
+	}
+}
